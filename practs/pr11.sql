b26fd79ed5d2384d7cf6d47f0a2aa8e2119713f6
crizmo <crizmo595@gmail.com>
2022-09-28 19:09:12 +0530

    🗼 : PLSQL base

---
+++ practs/pr11.sql
@@ -0,0 +1,19 @@
+-- PLSQL commands
+
+-- @block
+BEGIN
+dbms_output.put_line('Hello World!');
+END;
+/
+
+-- @block
+DECLARE
+a integer: = 10;
+b integer: = 20;
+c integer;
+BEGIN
+c: = a + b;
+dbms_output.put_line('C ='||c);
+END;
+/
+
